e43468ce63d7f86b09ee18abe7aa43374ee101f1
volta tumbleweed <you@example.com>
2018-08-15 15:08:58 -0700

    User model: add email field; Users route: scaffold login route

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 // Create schema
 const UserSchema = new Schema({
   name: { type: String, required: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
   avatar: { type: String },
   date: { type: Date, default: Date.now}
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../models/User');
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 // @route	GET api/users/test
 // @desc 	Tests users route
@@ -9,29 +10,70 @@ const gravatar = require('gravatar');
 router.get('/test', (req, res) => res.json({msg: 'Users works'}));
 
 
-// @route	POST api/users/test
-// @desc 	Tests users route
+// @route	POST api/users/register
+// @desc 	Register a new user
 // @access 	Public
 router.post('/register', (req, res) => {
   User.findOne({ email: req.body.email })
 	.then(user => {
 	  if (user) {
 		return res.status(400).json({email: 'Email already exists'})
-	}) else {
+	  } else {
 	
-	  const avatar = gravatar.url(req.body.email, {
-		s: '200', // size
-		r: 'pg', // rating
-		d: 'mm', // default: blank user icon
-	  });
+		const avatar = gravatar.url(req.body.email, {
+		  s: '200', // size
+		  r: 'pg', // rating
+		  d: 'mm', // default: blank user icon
+		});
 
-	  const newUser = new User({
-	 	name: req.body.name,
-		email: req.body.email,
-		avatar,
-		password: req.body.password
-	  });
-	}
+		const newUser = new User({
+		  name: req.body.name,
+		  email: req.body.email,
+		  avatar,
+		  password: req.body.password
+		});
+
+		bcrypt.genSalt(10, (err, salt) => {
+		  bcrypt.hash(newUser.password, salt, (err, hash) => {
+			if (err) throw err;
+			newUser.password = hash;
+			newUser
+			  .save()
+			  .then(user => res.json(user))
+			  .catch(err => console.log(err));
+		  })
+	    })
+
+	  }
+
+	});
 });
+
+
+// @route	POST api/users/login
+// @desc 	login a user 
+// @access 	Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email})
+	.then( user => {
+	  if (!user) {
+		return res.status(404).json({email: 'User not found'})
+	  } 
+	  
+	  // Check password
+	  bcrypt.compare(password, user.password)
+		.then(isMatch => {
+		  if (isMatch) {
+			res.json({msg: 'Success'})
+		  } else {
+			return res.status(400).json({password: 'Password incorrect'});
+		  }
+		});
+	})
+
+})
 
 module.exports = router;
