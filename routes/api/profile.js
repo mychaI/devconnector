7192a20a1e0c859ff12e0f88256ec5363eae87b0
volta tumbleweed <you@example.com>
2018-08-25 18:18:05 -0700

    Profile Validation: create profile field validations; Profile Route: add create/update functionality, integrate profile validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+// Load Validation 
+const validateProfileInput = require('../../validation/profile')
+
 // Load Models
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -19,22 +22,48 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
   const errors = {};
 
   Profile.findOne({ user: req.user.id })
+	.populate('user', ['name', 'avatar'])
 	.then( profile => {
 	  if (!profile) {
 		errors.noprofile = 'There is no profile for this user';
 		return res.status(404).json(errors);
 	  }
 	  res.json(profile);
-	})
-	.catch( err => {
-	  res.status(404).json(err)
 	});
+
 });
+
+// @route	GET api/profile/handle/:handle
+// @desc 	Get a user's profile by handle
+// @access 	Public
+router.get('/handle/:handle', (req,res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+	.populate('user', ['name', 'avatar' ])
+	.then( profile=> {
+	  if (!profile) {
+		errors.noprofile = 'There is no profile for this user';
+		res.status(404).json(errors);
+	  }
+	  res.json(profile);
+	})
+	.catch(err => res.status(404).json(err));
+
+});
+
 
 // @route	POST api/profile
 // @desc 	Create or Edit current user's profile
 // @access 	Private
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  // Validation
+  const { errors, isValid } = validateProfileInput(req.body);
+
+  if (!isValid) {
+	return res.status(400).json(errors);
+  }
 
   // Get fields and populate profileFields object
   const profileFields = {};
