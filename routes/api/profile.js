60123f54598bbc8a97c929798dd2485e3bd58518
volta tumbleweed <you@example.com>
2018-09-06 16:49:32 -0700

    Post model: create; Posts route: add basic post route; Post validation: create

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -251,9 +251,9 @@ router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: fal
 
 });
 
-//@route DELETE api/profile/education/:edu_id
-//@desc Delete education from profile
-//@access Private
+//@route   DELETE api/profile/education/:edu_id
+//@desc    Delete education from profile
+//@access  Private
 router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req, res) => {
 
   Profile.findOne({ user: req.user.id }).then( profile=> {
@@ -271,5 +271,17 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', { session: fals
   .catch(err => res.status(404).json(err));
 });
 
+//@route   DELETE api/profile
+//@desc    Delete user and profile
+//@access  Private
+router.delete('/', passport.authenticate('jwt', { session: false}), (req, res) => {
+  
+  Profile.findOneAndRemove({ user: req.user.id })
+  .then(()=> {
+	User.findOneAndRemove({ _id: req.user.id })
+	.then(() => res.json({ success: true }));	
+  });
+
+});
 
 module.exports = router;
