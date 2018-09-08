d30907a23040b035a47650d9c1525653281b25cc
volta tumbleweed <you@example.com>
2018-09-07 18:31:47 -0700

    Post route: make delete post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 
 // Load models
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 // Load validation
 const validatePostInput = require('../../validation/post');
@@ -13,6 +14,26 @@ const validatePostInput = require('../../validation/post');
 // @desc 	Tests post route
 // @access 	Public
 router.get('/test', (req, res) => res.json({msg:"posting works"}));
+
+// @routes  GET api/posts
+// @desc	Get posts
+// @access	Public
+router.get('/', (req, res) => {
+  Post.find()
+	.sort({date: -1})
+	.then(posts => res.json(posts))
+	.catch(err => res.status(404).json({ message: 'No posts found' }));
+});
+
+// @routes  GET api/posts/:id
+// @desc	Get a post by id
+// @access	Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+	.then(post => res.json(post))
+	.catch(err => res.status(404).json({ message: 'No post found with that ID' }));
+});
+
 
 // @routes  POST api/posts
 // @desc	Create a post
@@ -34,6 +55,25 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
   });
 
   newPost.save().then(post => res.json(post));
+});
+
+// @routes	DELETE api/post/:id
+// @desc	Delete post by id
+// @access 	Private
+router.delete('/:id', passport.authenticate('jwt',{ session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id })
+	.then(profile => {
+	  Post.findById(req.params.id)
+		.then(post => {
+		  // Check for post  owner
+		  if (post.user.toString() !== req.user.id) {
+			res.status(401).json({ notauthorized: 'User not authorized' });
+		  }
+		  // Delete post
+		  post.remove().then(() => res.json({ success: true }));
+		})
+		.catch(err => res.status(404).json({ message: 'Post not found' }));
+	})
 });
 
 module.exports = router;
