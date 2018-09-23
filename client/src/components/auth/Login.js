7e503fe1930edc79fbf13275f16fb2b590f9e44b
volta tumbleweed <you@example.com>
2018-09-22 19:03:50 -0700

    auth/Login Component: set up submit fnality

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+
+  constructor() {
+	super()
+	this.state = {
+	  email: '',
+	  password: '',
+	  errors: {}
+ 	}
+
+	// Bind this to self
+	this.onChange = this.onChange.bind(this)
+	this.onSubmit = this.onSubmit.bind(this)
+	
+  }
+
+  onChange(event) {
+	this.setState({ [event.target.name] : event.target.value })
+  }
+
+  onSubmit(event) {
+	event.preventDefault()
+	const login = {
+	  email: this.state.email,
+	  password: this.state.password
+	}
+	console.log(login)
+  }
+
   render() {
 	return (
 	  <div className='login'>
@@ -11,12 +39,24 @@ class Login extends Component {
 			  <h1 className='display-4 text-center'>Log In</h1>
 			  <p className='lead text-center'>Sign in to your DevConnector account</p>
 
-			  <form action='dashboard.html'>
+			  <form onSubmit={ this.onSubmit }>
 				<div className='form-group'>
-				  <input type='email' className='form-control form-control-lg' placeholder='Email Address' name='email' />
+				  <input type='email' 
+						className='form-control form-control-lg' 
+						placeholder='Email Address' 
+						name='email' 
+						value={ this.state.email }
+						onChange={ this.onChange }
+						/>
 				</div>
 				<div className='form-group'>
-				  <input type='password' className='form-control form-control-lg' placeholder='Password' name='password' />
+				  <input type='password' 
+						className='form-control form-control-lg' 
+						placeholder='Password' 
+						name='password' 
+						value={ this.state.password }
+						onChange={ this.onChange }
+						/>
 				</div>
 				<input type='submit' className='btn btn-info btn-block mt-4' />
 			  </form>
